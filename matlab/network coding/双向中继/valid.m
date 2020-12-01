24066af9479abbc61fd6a787a7179c517fd0ec5f
valbrea <15135406+valbrea@users.noreply.github.com>
2020-12-01 13:28:32 +0800

    更新valid

--- matlab/network coding/双向中继/valid.m
+++ matlab/network coding/双向中继/valid.m
@@ -1,7 +1,7 @@
 % 计算有效吞吐量
 function valid(SNR, slot)
 loop = 50;
-
+xaxis = 1 : 30 : slot;
 % A2_valid_throughput = zeros(loop, slot);
 A3_valid_throughput = zeros(loop, slot);
 DFNC4_valid_throughput = zeros(loop, slot);
@@ -12,26 +12,33 @@ A3_valid_mean = zeros(1, slot);
 DFNC4_valid_mean = zeros(1, slot);
 SNC4_valid_mean = zeros(1, slot);
 for i = 1 : 1 : loop
-%     A2_valid_throughput(i, :) = A2Network6(SNR, slot);
+    A2_valid_throughput(i, :) = A2Network6(SNR, slot);
     A3_valid_throughput(i, :) = A3Network6 (SNR, slot);
 %     A3_valid_throughput(i, :) = A3Network6_v2 (SNR, slot);
     DFNC4_valid_throughput(i, :) = DFNC4(SNR, slot);
     SNC4_valid_throughput(i, :) = SNC4(SNR, slot);
 end
 
-% A2_valid_mean = mean(A2_valid_throughput);
+A2_valid_mean = mean(A2_valid_throughput);
 A3_valid_mean = mean(A3_valid_throughput);
 DFNC4_valid_mean = mean(DFNC4_valid_throughput);
 SNC4_valid_mean = mean(SNC4_valid_throughput);
 
+A2_mean = A2_valid_mean(:, xaxis);
+A3_mean = A3_valid_mean(:, xaxis);
+DFNC4_mean = DFNC4_valid_mean(:, xaxis);
+SNC4_mean = SNC4_valid_mean(:, xaxis);
+
 figure, hold on;
-% plot (A2_valid_mean);
-plot (A3_valid_mean);
-plot (DFNC4_valid_mean);
-plot (SNC4_valid_mean);
-legend( '本算法', 'M-DF-NC', 'SNC-DM');
-xlabel('时隙');
-ylabel('有效吞吐量');
-title(['信噪比=', num2str(SNR), ', 循环次数=', num2str(loop)]);
+% , 'MarkerIndices', 1 : 60 : slot
+plot (xaxis, A3_mean, '-o');
+plot (xaxis, A2_mean, '-^');
+plot (xaxis, DFNC4_mean, '-d');
+plot (xaxis, SNC4_mean, '-s');
+
+legend( '本发明', '文献[4]', '文献[2]', '文献[3]');
+xlabel('仿真时隙数');
+ylabel('有效传输吞吐量');
+% title(['信噪比=', num2str(SNR), ', 循环次数=', num2str(loop)]);
 end
 
