700b6c2c19ebeec3f992a8158d3344211b7ca08e
yzmacbook <val.brea@qq.com>
2020-12-14 14:47:48 +0800

    ch7思考题测试

---
+++ cpp/Study_Practice/ch7/test.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+    while (i < 3)
+    {
+        switch (i++)
+        {
+            case 0: cout << "0 ";
+            case 1: cout << "1 ";
+            case 2: cout << "2 ";
+            default : cout << "oh no!";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
